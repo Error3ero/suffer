5df377814ac96dd38ab89d9510299afb9c886bf3
error3ero <emil.sadykov@gmail.com>
2023-12-23 19:22:46 +0300

    try to run

--- hw3/controller.v
+++ hw3/controller.v
@@ -1,7 +1,18 @@
 module decoder(opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop);
     input [5:0] opcode;
-    output reg memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
-    output reg [1:0] ALUop;
+    output wire memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
+    output wire [1:0] ALUop;
+
+    reg [7:0] toOut;
+
+    assign memtoreg = toOut[7];
+    assign memwrite = toOut[6];
+    assign branch = toOut[5];
+    assign ALUsrc = toOut[4];
+    assign regdst = toOut[3];
+    assign regwrite = toOut[2];
+    assign ALUop[1] = toOut[1];
+    assign ALUop[0] = toOut[0];
 
     supply0 zero;
     supply1 one;
@@ -9,44 +20,44 @@ module decoder(opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALU
     always begin
         case (opcode)
             6'b000000 : begin
-                memtoreg = 0;
-                memwrite = 0;
-                branch = 0;
-                ALUsrc = 0;
-                regdst = 1;
-                regwrite = 1;
-                ALUop[1] = 1;
-                ALUop[0] = 0;
+                toOut[7] = 0;
+                toOut[6] = 0;
+                toOut[5] = 0;
+                toOut[4] = 0;
+                toOut[3] = 1;
+                toOut[2] = 1;
+                toOut[1] = 1;
+                toOut[0] = 0;
                 end
             6'b100011 : begin
-                memtoreg = 1;
-                memwrite = 0;
-                branch = 0;
-                ALUsrc = 1;
-                regdst = 0;
-                regwrite = 1;
-                ALUop[1] = 0;
-                ALUop[0] = 0;
+                toOut[7] = 1;
+                toOut[6] = 0;
+                toOut[5] = 0;
+                toOut[4] = 1;
+                toOut[3] = 0;
+                toOut[2] = 1;
+                toOut[1] = 0;
+                toOut[0] = 0;
                 end
             6'b101011 : begin
-                memtoreg = 1'bx;
-                memwrite = 1;
-                branch = 0;
-                ALUsrc = 1;
-                regdst = 1'bx;
-                regwrite = 0;
-                ALUop[1] = 0;
-                ALUop[0] = 0;
+                toOut[7] = 0;
+                toOut[6] = 1'bx;
+                toOut[5] = 1;
+                toOut[4] = 0;
+                toOut[3] = 1;
+                toOut[2] = 1'bx;
+                toOut[1] = 0;
+                toOut[0] = 0;
                 end
             6'b000100 : begin
-                memtoreg = 1'bx;
-                memwrite = 0;
-                branch = 1;
-                ALUsrc = 0;
-                regdst = 1'bx;
-                regwrite = 0;
-                ALUop[1] = 0;
-                ALUop[0] = 1;
+                toOut[7] = 0;
+                toOut[6] = 1'bx;
+                toOut[5] = 0;
+                toOut[4] = 1;
+                toOut[3] = 0;
+                toOut[2] = 1'bx;
+                toOut[1] = 0;
+                toOut[0] = 1;
                 end
         endcase
     end
