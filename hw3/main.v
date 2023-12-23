7f4737f87ce99e6285e75b5655ca673b13a93835
error3ero <emil.sadykov@gmail.com>
2023-12-24 02:09:02 +0300

    try to run

--- hw3/main.v
+++ hw3/main.v
@@ -9,9 +9,11 @@ module main();
 
     reg clk;
 
+    wire b;
+
     decoder d(opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop);
 
-    test t(opcode, clk);
+    test t(opcode, b);
 
     initial begin
         opcode[5] = 0;
@@ -26,7 +28,7 @@ module main();
         $display("statement = %d", clk);
         $display("memtoreg = %d, memwrite = %d, branch = %d, ALUsrc = %d, regdst = %d, regwrite = %d, ALUop1 = %d, ALUop0 = %d", clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
-        $display("TESTS BITCH = %d", clk);
+        $display("TESTS BITCH = %d", b);
 
         opcode[5] = 1;
         opcode[4] = 0;
