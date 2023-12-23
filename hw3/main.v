dcf85c381ae8ca05801ccdc014b563a75ca09454
error3ero <emil.sadykov@gmail.com>
2023-12-24 01:58:42 +0300

    try to run

--- hw3/main.v
+++ hw3/main.v
@@ -21,7 +21,7 @@ module main();
         clk = 0;
         clk = 1;
         $display("memtoreg = %d, memwrite = %d, branch = %d, ALUsrc = %d, regdst = %d, regwrite = %d, ALUop1 = %d, ALUop0 = %d",
-        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0])
+        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
         opcode[5] = 1;
         opcode[4] = 0;
@@ -32,7 +32,7 @@ module main();
         clk = 0;
         clk = 1;
         $display("memtoreg = %d, memwrite = %d, branch = %d, ALUsrc = %d, regdst = %d, regwrite = %d, ALUop1 = %d, ALUop0 = %d",
-        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0])
+        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
         opcode[5] = 1;
         opcode[4] = 0;
@@ -43,7 +43,7 @@ module main();
         clk = 0;
         clk = 1;
         $display("memtoreg = %d, memwrite = %d, branch = %d, ALUsrc = %d, regdst = %d, regwrite = %d, ALUop1 = %d, ALUop0 = %d",
-        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0])
+        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
         opcode[5] = 0;
         opcode[4] = 0;
@@ -54,7 +54,7 @@ module main();
         clk = 0;
         clk = 1;
         $display("memtoreg = %d, memwrite = %d, branch = %d, ALUsrc = %d, regdst = %d, regwrite = %d, ALUop1 = %d, ALUop0 = %d",
-        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0])
+        clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
 
 
