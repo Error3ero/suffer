2a433a9b1e00fbc4de8b6607f82f37f7f4bd4650
error3ero <emil.sadykov@gmail.com>
2023-12-23 20:07:31 +0300

    try to run

--- hw3/controller.v
+++ hw3/controller.v
@@ -1,61 +1,52 @@
 module decoder(clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop);
     input clk;
-    input   [5:0] opcode;
-    output wire memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
-    output wire [1:0] ALUop;
+    input [5:0] opcode;
+    output reg memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
+    output reg [1:0] ALUop;
 
-    reg [7:0] toOut;
-
-    assign memtoreg = toOut[7];
-    assign memwrite = toOut[6];
-    assign branch = toOut[5];
-    assign ALUsrc = toOut[4];
-    assign regdst = toOut[3];
-    assign regwrite = toOut[2];
-    assign ALUop[1] = toOut[1];
-    assign ALUop[0] = toOut[0];
+    reg [7:0] ALUop;
 
     always @ (posedge clk) begin
         case (opcode)
             6'b000000 : begin
-                toOut[7] = 0;
-                toOut[6] = 0;
-                toOut[5] = 0;
-                toOut[4] = 0;
-                toOut[3] = 1;
-                toOut[2] = 1;
-                toOut[1] = 1;
-                toOut[0] = 0;
+                memtoreg = 0;
+                memwrite = 0;
+                branch = 0;
+                ALUsrc = 0;
+                regdst = 1;
+                regwrite = 1;
+                ALUop[1] = 1;
+                ALUop[0] = 0;
                 end
             6'b100011 : begin
-                toOut[7] = 1;
-                toOut[6] = 0;
-                toOut[5] = 0;
-                toOut[4] = 1;
-                toOut[3] = 0;
-                toOut[2] = 1;
-                toOut[1] = 0;
-                toOut[0] = 0;
+                memtoreg = 1;
+                memwrite = 0;
+                branch = 0;
+                ALUsrc = 1;
+                regdst = 0;
+                regwrite = 1;
+                ALUop[1] = 0;
+                ALUop[0] = 0;
                 end
             6'b101011 : begin
-                toOut[7] = 0;
-                toOut[6] = 1'bx;
-                toOut[5] = 1;
-                toOut[4] = 0;
-                toOut[3] = 1;
-                toOut[2] = 1'bx;
-                toOut[1] = 0;
-                toOut[0] = 0;
+                memtoreg = 0;
+                memwrite = 1'bx;
+                branch = 1;
+                ALUsrc = 0;
+                regdst = 1;
+                regwrite = 1'bx;
+                ALUop[1] = 0;
+                ALUop[0] = 0;
                 end
             6'b000100 : begin
-                toOut[7] = 0;
-                toOut[6] = 1'bx;
-                toOut[5] = 0;
-                toOut[4] = 1;
-                toOut[3] = 0;
-                toOut[2] = 1'bx;
-                toOut[1] = 0;
-                toOut[0] = 1;
+                memtoreg = 0;
+                memwrite = 1'bx;
+                branch = 0;
+                ALUsrc = 1;
+                regdst = 0;
+                regwrite = 1'bx;
+                ALUop[1] = 0;
+                ALUop[0] = 1;
                 end
         endcase
     end
--- hw3/main.v
+++ hw3/main.v
@@ -2,81 +2,60 @@
 `include "util.v"
 
 module main();
-    reg [3:0] b;
+    reg [5:0] opcode;
+    reg  memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
+    reg [1:0] ALUop;
+    reg clk;
 
-    wire a;
-
-    supply1 one;
-    supply0 zero;
-
-    assign a = b[3] ? one : zero;
+    decoder decoder1(clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop);
 
     initial begin
-        b[3] = 1;
-        b[0] = 0;
-        b[2] = 0;
-        $display("a = %d", a);
+        //opcode = 6'b000000;
+        opcode[5] = 0;
+        opcode[4] = 0;
+        opcode[3] = 0;
+        opcode[2] = 0;
+        opcode[1] = 0;
+        opcode[0] = 0;
+        clk = 1;
+        clk = 0;
+        $display("000000");
+        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
-        b[3] = 0;
-        b[0] = 1;
-        b[2] = 0;
-        $display("a = %d", a);
+        //opcode = 6'b100011;
+        opcode[5] = 1;
+        opcode[4] = 0;
+        opcode[3] = 0;
+        opcode[2] = 0;
+        opcode[1] = 1;
+        opcode[0] = 1;
+        clk = 1;
+        clk = 0;
+        $display("100011");
+        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
+
+        //opcode = 6'b101011;
+        opcode[5] = 1;
+        opcode[4] = 0;
+        opcode[3] = 1;
+        opcode[2] = 0;
+        opcode[1] = 1;
+        opcode[0] = 1;
+        clk = 1;
+        clk = 0;
+        $display("101011");
+        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
+
+        //opcode = 6'b000100;
+        opcode[5] = 0;
+        opcode[4] = 0;
+        opcode[3] = 0;
+        opcode[2] = 1;
+        opcode[1] = 0;
+        opcode[0] = 0;
+        clk = 1;
+        clk = 0;
+        $display("000100");
+        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
     end
-
-//    reg [5:0] opcode;
-//    wire  memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
-//    wire [1:0] ALUop;
-//    reg clk;
-//
-//    decoder decoder1(clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop);
-//
-//    initial begin
-//        //opcode = 6'b000000;
-//        opcode[5] = 0;
-//        opcode[4] = 0;
-//        opcode[3] = 0;
-//        opcode[2] = 0;
-//        opcode[1] = 0;
-//        opcode[0] = 0;
-//        clk = 1;
-//        clk = 0;
-//        $display("000000");
-//        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
-//
-//        //opcode = 6'b100011;
-//        opcode[5] = 1;
-//        opcode[4] = 0;
-//        opcode[3] = 0;
-//        opcode[2] = 0;
-//        opcode[1] = 1;
-//        opcode[0] = 1;
-//        clk = 1;
-//        clk = 0;
-//        $display("100011");
-//        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
-//
-//        //opcode = 6'b101011;
-//        opcode[5] = 1;
-//        opcode[4] = 0;
-//        opcode[3] = 1;
-//        opcode[2] = 0;
-//        opcode[1] = 1;
-//        opcode[0] = 1;
-//        clk = 1;
-//        clk = 0;
-//        $display("101011");
-//        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
-//
-//        //opcode = 6'b000100;
-//        opcode[5] = 0;
-//        opcode[4] = 0;
-//        opcode[3] = 0;
-//        opcode[2] = 1;
-//        opcode[1] = 0;
-//        opcode[0] = 0;
-//        clk = 1;
-//        clk = 0;
-//        $display("000100");
-//        $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
-//    end
 endmodule
