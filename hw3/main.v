84cff5acc30793763cf00ca6f9557424be373ce3
error3ero <emil.sadykov@gmail.com>
2023-12-23 19:29:32 +0300

    try to run

--- hw3/main.v
+++ hw3/main.v
@@ -9,25 +9,49 @@ module main();
     decoder decoder1(clk, opcode, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop);
 
     initial begin
-        opcode = 6'b000000;
+        //opcode = 6'b000000;
+        opcode[5] = 0;
+        opcode[4] = 0;
+        opcode[3] = 0;
+        opcode[2] = 0;
+        opcode[1] = 0;
+        opcode[0] = 0;
         clk = 1;
         clk = 0;
         $display("000000");
         $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
-        opcode = 6'b100011;
+        //opcode = 6'b100011;
+        opcode[5] = 1;
+        aopcode[4] = 0;
+        opcode[3] = 0;
+        opcode[2] = 0;
+        opcode[1] = 1;
+        opcode[0] = 1;
         clk = 1;
         clk = 0;
         $display("100011");
         $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
-        opcode = 6'b101011;
+        //opcode = 6'b101011;
+        opcode[5] = 1;
+        opcode[4] = 0;
+        opcode[3] = 1;
+        opcode[2] = 0;
+        opcode[1] = 1;
+        opcode[0] = 1;
         clk = 1;
         clk = 0;
         $display("101011");
         $display("memtoreg: %d, memwrite: %d, branch: %d, ALUsrc: %d, regdst: %d, regwrite: %d, ALUop: %d%d", memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUop[1], ALUop[0]);
 
-        opcode = 6'b000100;
+        //opcode = 6'b000100;
+        opcode[5] = 0;
+        opcode[4] = 0;
+        opcode[3] = 0;
+        opcode[2] = 1;
+        opcode[1] = 0;
+        opcode[0] = 0;
         clk = 1;
         clk = 0;
         $display("000100");
