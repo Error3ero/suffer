59e4bd2912c4479edfdf9b23250477950d704edb
error3ero <emil.sadykov@gmail.com>
2023-12-24 01:41:34 +0300

    try to run

--- hw3/main.v
+++ hw3/main.v
@@ -3,8 +3,16 @@
 
 module main();
     wire a;
-    reg [1:0] b = 2'b10;
-    assign a = b == 2'b00 ? 1 : 0;
+    assign a = 0;
+    wire b;
+    assign b = 1 == 0;
 
-    initial $display("%d", a);
+    initial $display("%d, %d", a, b);
+endmodule
+
+module test(a, out);
+    input a;
+    output [3:0];
+
+
 endmodule
