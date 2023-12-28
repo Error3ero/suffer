ac79184b8d4bd8beb2159c1161398984de0592ad
error3ero <emil.sadykov@gmail.com>
2023-12-28 23:49:47 +0300

    verilog is dumb

--- hw3/main.v
+++ hw3/main.v
@@ -1,12 +1,13 @@
 `include "controller.v"
 `include "ALU.v"
-`include "util.v"
 
 module main();
     reg [15:0] a = 16'b1010111010100001;
-    wire b = a == 16'b1010111010100001;
+    reg [15:0] b = 16'b1010111010100001;
+    wire c;
+    assign c = a == b ? 1 : 0;
 
     initial begin
-        $display("peins %d", b);
+        $display("peins %d", c);
     end
 endmodule
