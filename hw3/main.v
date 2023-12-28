07c6863e27f27550fcc91f77de2654742e4ead3a
error3ero <emil.sadykov@gmail.com>
2023-12-29 00:06:25 +0300

    verilog is dumb

--- hw3/main.v
+++ hw3/main.v
@@ -6,9 +6,51 @@ module main();
     wire b = 1;
 
     wire c;
-    assign c = (a == b) == (a == 1);
+
+    and_gate an(a, b, c);
     initial begin
         $display("peins %d", c);
     end
+endmodule
+
+module and_gate(a, b, out);
+  input wire a, b;
+  output wire out;
+
+  wire nand_out;
+
+  nand_gate nand_gate1(a, b, nand_out);
+  not_gate not_gate1(nand_out, out);
+endmodule
+
+module nand_gate(a, b, out);
+  input wire a, b;
+  output out;
+
+  supply1 pwr;
+  supply0 gnd;
+
+  wire nmos1_out;
+
+  pmos pmos1(out, pwr, a);
+  pmos pmos2(out, pwr, b);
+
+  nmos nmos1(nmos1_out, gnd, b);
+  nmos nmos2(out, nmos1_out, a);
+  // pmos pmos1(out, pwr, a);
+  // nmos nmos1(out, gnd, a);
+endmodule
+
+
+module not_gate(a, out);
+  input wire a;
+  output out;
+
+  supply1 pwr;
+  supply0 gnd;
+
+  // 1 - сток, 2 - исток, 3 - база
+  pmos pmos1(out, pwr, a);
+  nmos nmos1(out, gnd, a);
 endmodule
 
