eb2bc40f4f3232842663f5ca3f7e76765225f20c
error3ero <emil.sadykov@gmail.com>
2023-12-29 00:00:38 +0300

    verilog is dumb

--- hw3/main.v
+++ hw3/main.v
@@ -2,19 +2,12 @@
 `include "ALU.v"
 
 module main();
-    reg [15:0] a = 16'b1010111010100001;
-    reg [15:0] b = 16'b1010111010000001;
-    wire c;
+    wire a = 0;
+    wire b = 1;
 
-    test t(a, b, c);
+    wire c = a & b;
 
     initial begin
         $display("peins %d", c);
     end
-endmodule
-
-module test(a, b, out);
-  input [15:0] a, b;
-  output out;
-  assign out = a == b;
 endmodule
