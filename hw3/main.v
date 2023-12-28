d7df81ac0df0c7d11d6d22e14a93a83124f528cb
error3ero <emil.sadykov@gmail.com>
2023-12-28 23:58:05 +0300

    verilog is dumb

--- hw3/main.v
+++ hw3/main.v
@@ -4,10 +4,17 @@
 module main();
     reg [15:0] a = 16'b1010111010100001;
     reg [15:0] b = 16'b1010111010000001;
-    wire c = a == b;
+    wire c;
+
+    test t(a, b, c);
 
     initial begin
         $display("peins %d", c);
     end
 endmodule
 
+module test(a, b, out);
+  input [15:0] a, b;
+  output out;
+  assign out = a == b;
+endmodule
