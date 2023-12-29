bba0a8067c567126e1e10de54589d4846654d77b
error3ero <emil.sadykov@gmail.com>
2023-12-30 00:35:20 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -2,7 +2,7 @@
 `include "util.v"
 module main();
     reg [31:0] a = 32'b00000000000000000000000000000000;
-    reg [31:0] b;
+    wire [31:0] b;
     adder adder1(a, 4, b);
     initial begin
       $display("%d", b);
