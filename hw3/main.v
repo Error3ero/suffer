e704ff88dda1802d2c2f25c21f0f211f6ca0bb00
error3ero <emil.sadykov@gmail.com>
2023-12-30 01:06:16 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -5,8 +5,8 @@ module main();
     assign a[2] = 1;
     assign a[1] = 0;
     assign a[0] = 1;
-    wire [3:0] b;
-    assign b = {a[2:0], 1'b1};
+    wire [31:0] b;
+    assign b = {31{1'b0}, 1'b1};
     initial begin
       $display("%b", b);
     end
