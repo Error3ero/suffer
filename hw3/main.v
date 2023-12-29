1452ad51b87c293dfe2157d9383aceb8c78ec878
error3ero <emil.sadykov@gmail.com>
2023-12-30 01:44:01 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -4,8 +4,8 @@ module main();
     reg [5:0] opcode = 6'b000000;
     wire c1;
     assign c1 = opcode == 6'b000000;
-    wire b;
-    assign b = c1 ? 1 : 0;
+    wire b[1:0];
+    assign b = c1 ? 2'b01 : 2'b10;
 
 
 
