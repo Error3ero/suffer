611836657f4c1c6ed1f00d0f7f246a1ce42d87c4
error3ero <emil.sadykov@gmail.com>
2023-12-30 01:01:52 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -3,7 +3,7 @@
 module main();
     reg [15:0] a = 4;
     wire [31:0] b;
-    assign b = {16'b1111111111111111, 16'b0000000000000000};
+    assign b = {a, 16'b0000000000000000};
     //sign_extend s(a, b);
     initial begin
       $display("%b", b);
--- hw3/util.v
+++ hw3/util.v
@@ -13,8 +13,7 @@ module shl_2(in, out);
   input [31:0] in;
   output [31:0] out;
 
-  assign out[31:2] = in[29:0];
-  assign out[1:0] = 2'b00;
+  assign out = {in[29:0], 2'b00};
 endmodule
 
 // 32 битный сумматор
