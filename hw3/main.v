8fb9552189f5403df54e106d48355cd1aeaaacb4
error3ero <emil.sadykov@gmail.com>
2023-12-29 21:21:04 +0300

    verilog is dumb

--- hw3/main.v
+++ hw3/main.v
@@ -5,7 +5,7 @@ module main();
     reg [5:0] a = 6'b100101;
     reg [5:0] b = 6'b000000;
     wire c;
-    assign c = a == 6'b100101 && b == 6'b111111;
+    assign c = (a == 6'b100101) * (b == 6'b111111);
 
     initial begin
         $display("peins %d", c);
