87523b5cab169a81c2c237e7ddf1b7206b8ac6c0
error3ero <emil.sadykov@gmail.com>
2023-12-30 00:27:48 +0300

    cpu v0.01

--- hw3/pc.v
+++ hw3/pc.v
@@ -2,7 +2,8 @@ module pc(clk, pc_next, pc);
     input clk;
     input [31:0] pc_next;
     output reg [31:0] pc;
-    always @(posedge clk)
+    always @(posedge clk) begin
         pc = pc_next;
         $display("pc_next %b", pc_next);
+    end
 endmodule
