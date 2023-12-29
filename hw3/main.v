1c2924ec7c0d7576c7164eb988f8fa68995b7e58
error3ero <emil.sadykov@gmail.com>
2023-12-30 00:53:38 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -1,12 +1,10 @@
 //`include "controller.v"
 `include "util.v"
 module main();
-    reg [31:0] a = 4;
+    reg [16:0] a = 4;
     wire [31:0] b;
-    //shl_2 s(a, b);
-    assign b = {a[29:0], 2'b11};
+    sign_extend s(a, b);
     initial begin
-      $display("%b", a[29:0]);
       $display("%b", b);
     end
 endmodule
