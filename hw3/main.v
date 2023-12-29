2b0ee869c0e10683f2c6981a3819ae01cbfec947
error3ero <emil.sadykov@gmail.com>
2023-12-30 01:08:05 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -1,10 +1,7 @@
 //`include "controller.v"
 `include "util.v"
 module main();
-    wire [2:0] a;
-    assign a[2] = 1;
-    assign a[1] = 0;
-    assign a[0] = 1;
+    reg [31:0] a = 11;
     wire [31:0] b;
     shl_2 s(a, b);
     initial begin
