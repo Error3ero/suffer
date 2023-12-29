244bc9884b722c7822b5f2cbfdb7a0d06dbb9196
error3ero <emil.sadykov@gmail.com>
2023-12-30 00:35:38 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -1,7 +1,7 @@
 //`include "controller.v"
 `include "util.v"
 module main();
-    reg [31:0] a = 32'b00000000000000000000000000000000;
+    reg [31:0] a = 32'b00000000000000000000000000001000;
     wire [31:0] b;
     adder adder1(a, 4, b);
     initial begin
