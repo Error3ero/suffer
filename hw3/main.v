28347e73ef5d078ebba17e13680def1d2b5e8e2f
error3ero <emil.sadykov@gmail.com>
2023-12-30 01:10:28 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -6,7 +6,7 @@ module main();
     reg [29:0] interm = a[29:0];
     wire [31:0] b;
 //    shl_2 s(a, b);
-    assign b = {a[29:0], 2'b00};
+    assign b = {interm[29:0], 2'b00};
     initial begin
       $display("%b", b);
     end
