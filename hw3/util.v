fd38dbb16376ee02cb5a59f2ad21f1d9e74e754e
error3ero <emil.sadykov@gmail.com>
2023-12-30 01:23:21 +0300

    cpu v0.01

--- hw3/util.v
+++ hw3/util.v
@@ -13,8 +13,8 @@ module shl_2(in, out);
   input [31:0] in;
   output [31:0] out;
 
-  assign out = in << 2;
-  //assign out = {in[29:0], 2'b00};
+  //assign out = in << 2;
+  assign out = {in[29:0], 2'b00};
 endmodule
 
 // 32 битный сумматор
