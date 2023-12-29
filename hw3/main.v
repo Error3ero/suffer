65f05151a51ca9870858606e14223d395d31cf4e
error3ero <emil.sadykov@gmail.com>
2023-12-30 01:02:13 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -3,8 +3,7 @@
 module main();
     reg [15:0] a = 4;
     wire [31:0] b;
-    assign b = {a, 16'b0000000000000000};
-    //sign_extend s(a, b);
+    sign_extend s(a, b);
     initial begin
       $display("%b", b);
     end
