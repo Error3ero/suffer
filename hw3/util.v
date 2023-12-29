537c8150c8d687e67a28f9c461757f49a649bfec
error3ero <emil.sadykov@gmail.com>
2023-12-30 00:56:11 +0300

    cpu v0.01

--- hw3/util.v
+++ hw3/util.v
@@ -5,6 +5,23 @@ module sign_extend(in, out);
   output [31:0] out;
 
   assign out = {{16{in[15]}}, in};
+  assign out[15:0] = in;
+  assign out[16] = in[15];
+  assign out[17] = in[15];
+  assign out[18] = in[15];
+  assign out[19] = in[15];
+  assign out[20] = in[15];
+  assign out[21] = in[15];
+  assign out[22] = in[15];
+  assign out[23] = in[15];
+  assign out[24] = in[15];
+  assign out[25] = in[15];
+  assign out[26] = in[15];
+  assign out[27] = in[15];
+  assign out[28] = in[15];
+  assign out[29] = in[15];
+  assign out[30] = in[15];
+  assign out[31] = in[15];
 endmodule
 
 // модуль, который реализует побитовый сдвиг числа
