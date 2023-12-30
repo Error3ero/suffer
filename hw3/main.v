cbf4305dcd67531eb692a584cebae6035c65d049
error3ero <emil.sadykov@gmail.com>
2023-12-30 12:24:12 +0300

    cpu v0.01

--- hw3/main.v
+++ hw3/main.v
@@ -2,27 +2,27 @@
 //`include "util.v"
 module main();
   reg [5:0] opc = 6'b000000;
-  wire opcode, funct, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
+  wire funct, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite;
   wire [2:0] ALUcontrol;
   reg clk;
-  control c1(opc, opcode, funct, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUcontrol);
+  control c1(clk, opc, funct, memtoreg, memwrite, branch, ALUsrc, regdst, regwrite, ALUcontrol);
 
   initial begin
     #5;
     clk = 0;
     #5;
     clk = 1;
-    opcode = 6'b1000101;
+    opc = 6'b1000101;
     #5;
     clk = 0;
     #5;
     clk = 1;
-    opcode = 6'b1010101;
+    opc = 6'b1010101;
     #5;
     clk = 0;
     #5;
     clk = 1;
-    opcode = 6'b000100;
+    opc = 6'b000100;
   end
 endmodule
 
